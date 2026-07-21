c6b4c1f1408417add8730c9ead013d2e7552a6c8
oleg-insights <oleg.insights@mail.ru>
2026-07-22 00:32:58 +0300

    refactor(deploy): route traffic via host reverse proxy

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,6 @@ async function bootstrap() {
 
     SwaggerModule.setup('api-docs', app, document);
 
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3002);
 }
 void bootstrap();
